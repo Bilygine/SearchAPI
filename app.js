8ea21a59dabf486fe58e497af592de56fc6246f7
Kissou <adomou_b@etna-alternance.net>
2018-11-13 21:33:40 +0100

    datastore query

--- app.js
+++ app.js
@@ -1,48 +1,27 @@
 var express = require('express')
 var app = express()
 
+// SELECT * FROM Outputanalyzer WHERE oa_analyze_id="azbrtgerfdez"
+const Datastore = require('@google-cloud/datastore')
+const projectId = 'etna-gpe'
+
+const datastore = new Datastore({
+	projectId: projectId,
+})
+
 app.get('/', function (req, res) {
-    res.json({
-        message: 'Bilygine API Endpoint'
-    });
-});
-app.get('/analyze/:analyzeid', function (req, res) {
-  let id = req.params.analyzeid
   // SELECT * FROM Outputanalyzer WHERE oa_analyze_id="azbrtgerfdez"
-  res.json({
-      "id_sent": id
-  });
-});
-app.get('/search/:occurence', function (req, res) {
-  let occurence = req.params.occurence
-  console.log('[GET] /search/' + occurence)
-  //TODO: récupérer les données sur Datastore
-    res.json([
-      {
-        analyzeId: "azezaef",
-        url: "https://youtube.com/azezaeza",
-        timestamp: [3, 10, 234, 789, 1232]
-      },
-      {
-        analyzeId: "azezaef",
-        url: "https://youtube.com/azezaeza",
-        timestamp: [3, 10, 234, 789, 1232]
-      },
-      {
-        analyzeId: "azezaef",
-        url: "https://youtube.com/azezaeza",
-        timestamp: [3, 10, 234, 789, 1232]
-      }
-    ]);
-});
+	res = datastore.createQuery('')
+      .hasAncestor(datastore.key(['oa_analyzeid', "trjtryjzrtyzht"]))
+})
 
 // API Routes
-let router = express.Router();
+let router = express.Router()
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Content-Type", "application/json")
-    next();
-  });
+    next()
+  })
 
 app.listen(3000)
